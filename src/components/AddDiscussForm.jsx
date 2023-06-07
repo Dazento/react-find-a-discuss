698b08299d8f0fe2d1fd19735633ab69c4622d36
dbury <dbury@eol.fr>
2023-06-07 14:53:37 +0200

    addDiscuss completed

--- src/components/AddDiscussForm.jsx
+++ src/components/AddDiscussForm.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { doc, increment, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../db/Firebase";
+// import { v4 as uuid } from "uuid";
 
 export const AddDiscussForm = () => {
+  const [value, setValue] = useState("");
+  let index = 0
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addQuestion(value);
+  };
+
+  const addQuestion = (question) => {
+    const docRef = doc(db, "questions", `${uuid()}`);
+
+    setDoc(docRef, {
+      question: question,
+      active: true,
+    });
+  };
+
   return (
-    <form>
-        <input className='input'/>
+    <form onSubmit={handleSubmit}>
+      <input
+        className="input"
+        type="text"
+        placeholder="Add a question"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
-  )
-}
+  );
+};
--- src/pages/AddDiscuss.jsx
+++ src/pages/AddDiscuss.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigation } from "../components/navigation/Navigation";
+import { AddDiscussForm } from "../components/AddDiscussForm";
 
 const AddDiscuss = () => {
   return (
     <div>
       <Navigation />
       <h1>Discuss</h1>
+      <AddDiscussForm />
     </div>
   );
 };
